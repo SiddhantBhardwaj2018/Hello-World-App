9446d56efd2081e94b32dbeb0f25561160cd9f7e
Siddhant Bhardwaj <bhardwajsiddhant03@gmail.com>
2024-11-28 10:52:13 +0530

    first commit

---
+++ app/src/main/java/com/siddhantbhardwaj/helloworld/MainActivity.java
@@ -0,0 +1,54 @@
+package com.siddhantbhardwaj.helloworld;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        if(itemId == R.id.action_home){
+            Toast.makeText(
+                    this,"You selected home",Toast.LENGTH_SHORT
+            ).show();
+        }else{
+            Toast.makeText(
+                    this,"You selected search",Toast.LENGTH_SHORT
+            ).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
